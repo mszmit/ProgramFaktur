e9c2f10a520ceb18c4939f55ff127933c75ee30e
Szmit Marcin <mszmitek@gmail.com>
2013-10-14 12:24:49 +0200

    initial project version 0.01

---
+++ src/Frame.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+
+public class Frame {
+
+	public static void main(String[] args) {
+		
+		SimpleFrame frame = new SimpleFrame();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setVisible(true);
+
+	}
+
+}
---
+++ src/SimpleFrame.java
@@ -0,0 +1,13 @@
+import javax.swing.JFrame;
+
+
+public class SimpleFrame extends JFrame{
+
+	public SimpleFrame()
+	{
+		setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
+		setTitle("Program Faktur");
+	}
+	public static final int DEFAULT_WIDTH = 300;
+	public static final int DEFAULT_HEIGHT = 200;
+}
